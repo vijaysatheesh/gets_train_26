1ab5f3120bc2271e89998637c8570c7d60b4f8ff
george <geo262004@gmail.com>
2026-06-11 13:34:21 +0530

    removed everthing

--- main.c
+++ main.c
@@ -3,16 +3,5 @@
 int main(int argc, char const *argv[])
 {
 
-   printf("Vijay Satheesh\n");
-
-	printf("My name is George MT\n");
-    printf("This is added by vijay");
-    printf("Change 1");
-    printf("Change 2");
-    printf("Change 3");
-    printf("This is added by vijay\n");
-    return 0;
-
-	printf("Who are yoou???\n")
 
 }
