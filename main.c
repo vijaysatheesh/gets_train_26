4247bd607ff19bde7374d6947b0f5dd06ffda240
george <geo262004@gmail.com>
2026-06-11 12:51:07 +0530

    Added a string infront of george

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main(int argc, char const *argv[])
 
    printf("Vijay Satheesh\n");
 
-	printf("George MT\n");
-
+	printf("My name is George MT\n");
     printf("This is added by vijay");
     printf("Change 1");
     printf("Change 2");
     printf("Change 3");
+    printf("This is added by vijay\n");
     return 0;
 }
